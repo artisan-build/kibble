a71fcf12adaf29e684ed20074414fe4cd5d8288d
Len Woodward <Len@ProjektGopher.com>
2025-08-07 23:06:04 -0700

    make gh username and org configurable using env vars in kibble

--- config/kibble.php
+++ config/kibble.php
@@ -2,9 +2,9 @@
 
 return [
     'path' => base_path('packages'),
-    'organization' => 'artisan-build',
+    'organization' => env('GH_ORGANIZATION', 'artisan-build'),
     'template' => 'artisan-build/skeleton',
     'homepage' => 'https://artisan.community',
     'github_token' => env('GH_TOKEN'),
-    'github_username' => 'edgrosvenor',
+    'github_username' => env('GH_USERNAME', 'edgrosvenor'),
 ];
